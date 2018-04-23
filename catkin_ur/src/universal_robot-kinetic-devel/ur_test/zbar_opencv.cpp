d470de36a00fa935750ec139c630149e8fdddcdf
bjrhyqh <bjrhyqh@mail.ustc.edu.cn>
2018-04-23 14:35:32 +0800

    20180423

---
+++ catkin_ur/src/universal_robot-kinetic-devel/ur_test/zbar_opencv.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "zbar_opencv");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world!");
+}
---
+++ catkin_ur/src/ur5_robotiq_parallel-master/ur5_robotiq_parallel_bringup/scripts/pick_test2.py
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "pick_test2");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world!");
+}
